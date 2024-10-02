e3e3c2be05e9d55e0aa806124b2af5ee343080c1
Oleg Onyshko <olegonyshk00@gmail.com>
2024-10-02 13:59:43 +0300

    Add Photo

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFFFFF8F0),
-        body: Column(
+        body: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
-          
           children: [
-
-            SizedBox(height: 32,),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,  
-              children: [
+            Expanded(
+              child: Column( 
                 
-                Row(
+                children: [
+              
+                  SizedBox(height: 32,),
                   
-                  children: [
-                    Icon(Icons.call),
-                    Text('+380977491471; +380688515945')
-                    
-                  ],
-                  
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.alternate_email),
-                    Text('BoberKurwa@gmail.com')
-                  ]
-                )
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,  
+                    children: [
+                      
+                      Row(
+                        
+                        children: [
+                          Icon(Icons.call),
+                          Text('+380977491471; +380688515945')
+                      
+                          
+                        ],
+                        
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.alternate_email),
+                          Text('BoberKurwa@gmail.com')
+                        ]
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 48,),
+                  Text("Hi, we are Oleg Onyshko and Vlad Tsenglevich", style: TextStyle(fontSize: 16, color: Color(0xFF291854))),
+                  Text("Hello, we are students and study IT", style: TextStyle(fontSize: 48, color: Color(0xFF190D37), fontWeight: FontWeight.bold)),
+                  SizedBox(height: 32),
+                  Text("At first dreams seem impossible, then improbable, then inevitable. Christopher Reeve", style: TextStyle(fontSize: 18, color: Color(0xFF291854))),
+                ],
+              ),
             ),
-            SizedBox(height: 48,),
-            Text("Hi, we are Oleg Onyshko and Vlad Tsenglevich", style: TextStyle(fontSize: 16, color: Color(0xFF291854))),
-            Text("Hello, we are students and study IT", style: TextStyle(fontSize: 48, color: Color(0xFF190D37), fontWeight: FontWeight.bold)),
-            SizedBox(height: 32),
-            Text("At first dreams seem impossible, then improbable, then inevitable. Christopher Reeve", style: TextStyle(fontSize: 18, color: Color(0xFF291854))),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(200)),
+                child: Image.asset("assets/cat.jpg", height: 600, fit: BoxFit.cover)),)
+            
           ],
         ),
       ),
